25238be8998fdc356c18e1e00a8d60948b82ef05
vonacht <vonachtf80@gmail.com>
2026-01-08 01:14:18 +0100

    added support for floodfillpillars

--- json_builder.py
+++ json_builder.py
@@ -1,8 +1,93 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import copy
 import json
 
 from uassetgen import JSON_to_uasset
+
+@dataclass 
+class PillarRange:
+    min: float 
+    max: float 
+
+    @classmethod 
+    def from_dict(cls, data: dict): 
+        json_to_dataclass_map = {
+                "Max": "max",
+                "Min": "min"
+        }
+        adjusted_dict = {json_to_dataclass_map[k]:v for k, v in data.items()}
+        return cls(**adjusted_dict)
+
+@dataclass 
+class Location:
+    x: float 
+    y: float 
+    z: float
+
+    @classmethod 
+    def from_dict(cls, data: dict): 
+        json_to_dataclass_map = {
+                "X": "x",
+                "Y": "y",
+                "Z": "z"
+        }
+        adjusted_dict = {json_to_dataclass_map[k]:v for k, v in data.items()}
+        return cls(**adjusted_dict)
+
+@dataclass 
+class PillarPoint:
+    location: Location
+    points_range: PillarRange = field(default_factory=lambda: PillarRange(min=100, max=100))
+    noise_range: PillarRange = field(default_factory=lambda: PillarRange(min=150, max=150))
+    skew_factor: PillarRange = field(default_factory=lambda: PillarRange(min=0, max=0))
+    fill_amount: PillarRange = field(default_factory=lambda: PillarRange(min=100, max=100))
+
+    @classmethod 
+    def from_dict(cls, data: dict): 
+        json_to_dataclass_map = {
+                "Location": "location",
+                "Range": "points_range",
+                "FillAmount": "fill_amount",
+                "SkewFactor": "skew_factor",
+                "NoiseRange": "noise_range"
+        }
+        builder_map = {
+                "Location": Location.from_dict,
+                "Range": PillarRange.from_dict,
+                "FillAmount": PillarRange.from_dict,
+                "SkewFactor": PillarRange.from_dict,
+                "NoiseRange": PillarRange.from_dict
+        }
+        adjusted_dict = {json_to_dataclass_map[k]:builder_map[k](v) for k, v in data.items()}
+        return cls(**adjusted_dict)
+
+@dataclass 
+class FloodFillPillar:
+    points: list[PillarPoint]
+    range_scale: PillarRange = field(default_factory=lambda: PillarRange(min=1, max=1))
+    noise_range_scale: PillarRange = field(default_factory=lambda: PillarRange(min=1, max=1))
+
+    @classmethod 
+    def from_dict(cls, data: dict): 
+        json_to_dataclass_map = {
+                "Points": "points",
+                "RangeScale": "range_scale",
+                "NoiseRangeScale": "noise_range_scale"
+        }
+        builder_map = {
+                "Points": PillarPoint.from_dict,
+                "RangeScale": PillarRange.from_dict,
+                "NoiseRangeScale": PillarRange.from_dict
+                }
+
+        adjusted_dict = {}
+        for k, v in data.items():
+            if k == "Points":
+                adjusted_dict["points"] = [builder_map["Points"](point) for point in v]
+            else:
+                adjusted_dict[json_to_dataclass_map[k]] = builder_map[k](v)
+        return cls(**adjusted_dict)
+
 
 @dataclass
 class FloodFillLine:
@@ -104,13 +189,44 @@ def generate_entrance(de, entrance, num, outer_index):
     new_entrance["CreateBeforeCreateDependencies"] = [outer_index]
     return new_entrance
 
+def generate_pillar_point(dpp: dict, pillar_point: PillarPoint) -> dict:
+    new_pillar_point = copy.deepcopy(dpp)
+    new_pillar_point["Value"][0]["Value"][0]["Value"]["X"] = pillar_point.location.x
+    new_pillar_point["Value"][0]["Value"][0]["Value"]["Y"] = pillar_point.location.y
+    new_pillar_point["Value"][0]["Value"][0]["Value"]["Z"] = pillar_point.location.z
+    new_pillar_point["Value"][1]["Value"][0]["Value"] = pillar_point.points_range.min
+    new_pillar_point["Value"][1]["Value"][1]["Value"] = pillar_point.points_range.max
+    new_pillar_point["Value"][2]["Value"][0]["Value"] = pillar_point.noise_range.min
+    new_pillar_point["Value"][2]["Value"][1]["Value"] = pillar_point.noise_range.max
+    new_pillar_point["Value"][3]["Value"][0]["Value"] = pillar_point.skew_factor.min
+    new_pillar_point["Value"][3]["Value"][1]["Value"] = pillar_point.skew_factor.max
+    new_pillar_point["Value"][4]["Value"][0]["Value"] = pillar_point.fill_amount.min
+    new_pillar_point["Value"][4]["Value"][1]["Value"] = pillar_point.fill_amount.max
+
+    return new_pillar_point
+
+def generate_floodfillpillar(dp, dpp, pillar, num, outer_index):
+    new_pillar = copy.deepcopy(dp)
+    new_pillar["Data"][0]["Value"] = [generate_pillar_point(dpp, p) for p in pillar.points]
+    # RangeScale:
+    new_pillar["Data"][1]["Value"][0]["Value"] = pillar.range_scale.min
+    new_pillar["Data"][1]["Value"][1]["Value"] = pillar.range_scale.max
+    # NoiseRangeScale:
+    new_pillar["Data"][2]["Value"][0]["Value"] = pillar.noise_range_scale.min
+    new_pillar["Data"][2]["Value"][1]["Value"] = pillar.noise_range_scale.max
+    new_pillar["ObjectName"] = f"FloodFillPillar_{num}"
+    new_pillar["OuterIndex"] = outer_index
+    new_pillar["CreateBeforeCreateDependencies"] = [outer_index]
+    return new_pillar
+
 def parse_room_json(room_json: dict) -> tuple:
     floodfilllines = []
     for ffill in room_json["FloodFillLines"]:
         line = [FloodFillLine.from_dict(l) for l in room_json["FloodFillLines"][ffill]["Points"]]
         floodfilllines.append(line)
     entrances = [Entrance.from_dict(v) for _, v in room_json["Entrances"].items()]
-    return floodfilllines, entrances
+    pillars = [FloodFillPillar.from_dict(v) for _, v in room_json["FloodFillPillars"].items()]
+    return floodfilllines, entrances, pillars
 
 
 def generate_random_selector(drs: dict, drsr: dict, selector_refs: list, asset_list: list, outer_index: int) -> tuple[dict, list]:
@@ -157,7 +273,7 @@ def generate_room(dr, drr, tags, bounds, outer_index, name, selector_idx_list):
 
 def build_json_and_uasset(room_json: dict):
 
-    FLOODFILLLINES, ENTRANCES = parse_room_json(room_json)
+    FLOODFILLLINES, ENTRANCES, PILLARS = parse_room_json(room_json)
     ROOM_NAME = room_json["Name"]
     TAGS = room_json["Tags"]
     BOUNDS = room_json["Bounds"]
@@ -166,9 +282,13 @@ def build_json_and_uasset(room_json: dict):
         random_sel_num = 0
     else:
         random_sel_num = len(room_json["RandomSelectors"])
-    OUTER_ROOM_INDEX = len(room_json["FloodFillLines"]) + len(room_json["Entrances"]) + random_sel_num + 1
+    if room_json.get("FloodFillPillars") is None:
+        pillars_num  = 0
+    else:
+        pillars_num  = len(room_json["FloodFillPillars"])
+    OUTER_ROOM_INDEX = len(room_json["FloodFillLines"]) + len(room_json["Entrances"]) + random_sel_num + pillars_num + 1
 
-    asset_list = [k for k in room_json["FloodFillLines"]] + [k for k in room_json["Entrances"]]
+    asset_list = [k for k in room_json["FloodFillLines"]] + [k for k in room_json["Entrances"]] + [k for k in room_json["FloodFillPillars"]]
 
     with open("assets/default_assets/default_floodfillline.json", "r") as f:
         default_floodfillline = json.load(f)
@@ -186,6 +306,10 @@ def build_json_and_uasset(room_json: dict):
         default_random_selector = json.load(f)
     with open("assets/default_assets/default_randomselector_reference.json", "r") as f:
         default_random_selector_reference = json.load(f)
+    with open("assets/default_assets/default_pillar_point.json", "r") as f:
+        default_pillar_point = json.load(f)
+    with open("assets/default_assets/default_pillar.json", "r") as f:
+        default_pillar = json.load(f)
 
     floodfill_list = [
         generate_floodfill(
@@ -200,6 +324,10 @@ def build_json_and_uasset(room_json: dict):
     entrance_list = [
         generate_entrance(default_entrance, e, ii, OUTER_ROOM_INDEX)
         for ii, e in enumerate(ENTRANCES)
+    ]
+    pillar_list = [
+        generate_floodfillpillar(default_pillar, default_pillar_point, p, ii, OUTER_ROOM_INDEX)
+        for ii, p in enumerate(PILLARS)
     ]
 
     selector_list = []
@@ -224,6 +352,8 @@ def build_json_and_uasset(room_json: dict):
         default_asset["Exports"].append(ffill)
     for entrance in entrance_list:
         default_asset["Exports"].append(entrance)
+    for pillar in pillar_list:
+        default_asset["Exports"].append(pillar)
     for selector in selector_list:
         default_asset["Exports"].append(selector)
     default_asset["Exports"].append(room)
